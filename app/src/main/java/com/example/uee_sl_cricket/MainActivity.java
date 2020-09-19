595f917d6ffe87316cccb6e70ed6574e8caf2233
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-09-19 17:21:05 +0530

    home page clickable icons added

--- app/src/main/java/com/example/uee_sl_cricket/MainActivity.java
+++ app/src/main/java/com/example/uee_sl_cricket/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.uee_sl_cricket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +19,16 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
     }
+
+    public void methodNotification(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"This Is Notification",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void methodHome(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"This Is Home",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
 }
