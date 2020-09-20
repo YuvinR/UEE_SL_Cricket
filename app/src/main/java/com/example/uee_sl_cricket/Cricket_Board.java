40bf5092b75e5aee52b5e8d5b1504897d5d2cd43
Dewsara <dewsara6@gmail.com>
2020-09-20 16:32:23 +0530

    Notification and some New Activity added.

---
+++ app/src/main/java/com/example/uee_sl_cricket/Cricket_Board.java
@@ -0,0 +1,32 @@
+package com.example.uee_sl_cricket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class Cricket_Board extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+        setContentView(R.layout.activity_cricket__board);
+    }
+
+    public void methodNotification(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"This Is Notification",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void methodHome(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"This Is Home",Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
