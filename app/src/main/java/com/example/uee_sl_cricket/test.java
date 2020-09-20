c52670bd923d59b09c7dc6f87aa70315ca74bff7
Tharindu Wickramanayake <36078972+tdwic@users.noreply.github.com>
2020-09-20 13:46:57 +0530

    tst comit

---
+++ app/src/main/java/com/example/uee_sl_cricket/test.java
@@ -0,0 +1,14 @@
+package com.example.uee_sl_cricket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
