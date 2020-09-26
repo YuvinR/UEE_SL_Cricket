a729e61be0ffeb27aae90fdc8bdbab50c1b79881
Ovi98 <ovij98@gmail.com>
2020-09-26 07:54:52 +0530

    Gallery UI and Teams UI(minor changes)

---
+++ app/src/main/java/com/example/uee_sl_cricket/gallery.java
@@ -0,0 +1,32 @@
+package com.example.uee_sl_cricket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class gallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_gallery);
+    }
+
+    public void methodNotification(View view) {
+        Intent intent = new Intent(this, Notification.class);
+        startActivity(intent);
+    }
+
+    public void methodHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
